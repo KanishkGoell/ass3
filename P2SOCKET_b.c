fea790d3cb17cd3187c5f3e17cc7841e80a3a76b
KanishkGoell <83371955+KanishkGoell@users.noreply.github.com>
2022-12-20 20:46:11 +0530

    Update P2SOCKET_b.c

--- P2SOCKET_b.c
+++ P2SOCKET_b.c
@@ -1,28 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<sys/un.h>
-#include<unistd.h>
-#include<time.h>
-struct sockaddr_un servername;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
 #define SOCKET_NAME "CustomSocket"
-int main(int argc, char* argv[]){
-    socket(AF_UNIX, SOCK_SEQPACKET, 0);
-    int socket_id, data_socket;
-    memset(&servername, 0, sizeof(servername));
-    servername.sun_family = AF_UNIX;
+#define BUFFER_SIZE 12
+
+int main(int argc, char *argv[]) {
+	struct sockaddr_un servername;
+	int down_flag = 0;
+	int connection_socket;
+	int ret;
+	int maxIdx = 0;
+	int data_socket;
+	char buffer[BUFFER_SIZE];
+
+	socket(AF_UNIX, SOCK_SEQPACKET, 0);
+
+	memset(&servername, 0, sizeof(servername));
+	servername.sun_family = AF_UNIX;
 	strncpy(servername.sun_path, SOCKET_NAME, sizeof(servername.sun_path) - 1);
-    bind(socket_id, (const struct sockaddr *) &servername, sizeof(servername));
-    listen(socket_id, 100);
-    int maxIdx = 0,down_flag = 0;
-    char buffer[12];
-    for(;;) {
-		accept(socket_id, NULL, NULL);
+
+	bind(connection_socket, (const struct sockaddr *) &servername, sizeof(servername));
+
+	listen(connection_socket, 100);
+
+
+	while(1) {
+	    accept(connection_socket, NULL, NULL);
+
+
 		maxIdx = 0;
 		int curIdx = maxIdx;
 		while(1) {
 			read(data_socket, buffer, sizeof(buffer));
+
 
 			buffer[sizeof(buffer) - 1] = 0;
 			curIdx = buffer[0];
@@ -31,6 +45,7 @@ int main(int argc, char* argv[]){
 				down_flag = 1;
 				break;
 			}
+
 			//buffer contains the string with the 0th idx as the index of the string
 			printf("STRING SENT BY CLIENT: ");		
 			
@@ -50,16 +65,17 @@ int main(int argc, char* argv[]){
 			if(curIdx >= 50) break;
 		}
 		
-        if(down_flag) {
+		
+
+		close(data_socket);
+
+		if(down_flag) {
 			printf("SHUTTING SERVER");
-			close(socket_id);
+			close(connection_socket);
 			unlink(SOCKET_NAME);
 			exit(EXIT_SUCCESS);
 			break;
 		}
-		
-
-		close(data_socket);
 		
 	}
 
